845d2928947de0e4bd5ec1dbd1a1b9de38164264
Tran Dong Tran <tran-dong.tran@open-xchange.com>
2020-09-17 11:51:38 +0200

    Fix call-history re-opening after filtering
    Root Cause: after filtering mousedown on button closed dropdown, 'mouseup' (=click) opened it again
    Solution: prevent closing on mousedown, only allow closing on click
    
    + unskip e2e-test 'Call history closes on second click'

--- ui/e2e/tests/switchboard/calls_test.js
+++ ui/e2e/tests/switchboard/calls_test.js
@@ -218,8 +218,7 @@ Scenario('Call history is not visible when empty', (I, users) => {
     I.waitForVisible('.dropdown.open .call-history-item');
 });
 
-// TODO: fix behaviour in code
-Scenario.skip('Call history closes on second click', (I, users) => {
+Scenario('Call history closes on second click', (I, users) => {
 
     const [user1, user2] = users;
     const { primaryEmail, display_name } = user2.userdata;
